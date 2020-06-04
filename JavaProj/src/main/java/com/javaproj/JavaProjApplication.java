148ef3252c85caa527928d26894b6a47f034fed0
Rochellecanque <rvcanque@gmail.com>
2020-06-04 17:19:32 +0800

    Initial files for JavaProj application.

---
+++ JavaProj/src/main/java/com/javaproj/JavaProjApplication.java
@@ -0,0 +1,13 @@
+package com.javaproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjApplication.class, args);
+	}
+
+}
---
+++ JavaProj/src/test/java/com/javaproj/JavaProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
